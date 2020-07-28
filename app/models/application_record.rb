1e14c6cf54f077ece3fce90778d219fcd88613b1
h762 <11020i11020@gmail.com>
2020-07-28 21:37:13 +0900

    setting activehash

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
+class Brand < AvctiveHash::Base
+end
+
 class ApplicationRecord < ActiveRecord::Base
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :contry
   self.abstract_class = true
 end
