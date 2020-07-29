fab44f7321beade075a923da552717d05d86c62a
h762 <11020i11020@gmail.com>
2020-07-29 18:25:29 +0900

    put all relations about items
    Scss files of productListingPages had inserted productListingPage file.

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,8 +10,20 @@ class Item < ApplicationRecord
     福岡県:40,佐賀県:41,長崎県:42,熊本県:43,大分県:44,宮崎県:45,鹿児島県:46,沖縄県:47
   }
 
+
   has_many :images, dependent: :destroy
+  belongs_to :category
+  belongs_to :brand
+  belongs_to_active_hash :delivery_cost
+  belongs_to_active_hash :item_condition
+  belongs_to_active_hash :seller_region
+  belongs_to_active_hash :preparation_for_shipment
+  belongs_to_active_hash :status
+  belongs_to :seller, class_name: "User"
+  belongs_to :buyer, class_name: "User"
+
   accepts_nested_attributes_for :images, allow_destroy: true
+  
   with_options presence: true do 
     validates :name
     validates :decription
