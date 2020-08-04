e4f62634b15de7c969d4ef80276ee7ad3ea97685
h762 <11020i11020@gmail.com>
2020-08-04 17:10:42 +0900

    use active hash of delivery_cost

--- app/models/delivery_cost.rb
+++ app/models/delivery_cost.rb
@@ -1,6 +1,7 @@
 class DeliveryCost < ActiveHash::Base
   include ActiveHash::Associations
-  field :delivery_cost
-  add id: 1, payer: "送料込み(出品者負担)"
-  add id: 2, payer: "着払い(購入者負担)"
+  field :deliveryCost
+  add id: 0, deliveryCost: "選択してください"
+  add id: 1, deliveryCost: "送料込み(出品者負担)"
+  add id: 2, deliveryCost: "着払い(購入者負担)"
 end
