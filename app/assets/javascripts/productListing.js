6bd7c182b748f525bc0a3b226ac457b8f6e83b8c
h762 <11020i11020@gmail.com>
2020-08-06 11:22:07 +0900

    set auto calculator to fee and profit

--- app/assets/javascripts/productListing.js
+++ app/assets/javascripts/productListing.js
@@ -2,5 +2,16 @@ $(function(){
   $(".Base__body__area__item__informationArea__space").on('keyup', function(){
     $(".Base__body__area__item__informationArea__number").text($(this).val().length + "/1000");
   });
-  
+
+  $("#price").change(function(){
+    var price = $("#price").val();
+    var priceI = parseInt(price);
+    var fee = priceI / 10;
+    var profit = priceI - fee
+    var feeRadix = parseInt(fee, 10);
+    var profitRadix = parseInt(profit, 10);
+
+    $(".Base__body__area__price__chargeArea__calculate").html(feeRadix);
+    $(".Base__body__area__price__profitArea__calculate").html(profitRadix);
+  });
 })
